e5b868431a270515ab198a2098bed2993fc7d9a5
Shruti Chaudhary <chaudharyshruti326@gmail.com>
2026-06-23 22:13:31 +0530

    Add custom exceptions hierarchy for validation and error handling

--- exceptions.py
+++ exceptions.py
@@ -1,21 +1,25 @@
 class FinanceError(Exception):
     """Handles all the errors related to financial transactions in your personal finance tracker"""
     pass
+
 class  InvalidAmountError(FinanceError):
     """Raised when the amount entered for transaction is not valid."""
     def __init__(self, amount: float):
         self.amount=amount
         super().__init__(f"Amount must be greater than zero. Got: {amount}")
+
 class InvalidCategoryError(FinanceError):
     """Raised when the category entered is not valid."""
     def __init__(self, category: str):
         self.category=category
         super().__init__(f"Wrong category entered. You entered-{category}")
+
 class TransactionNotFoundError(FinanceError):
     """Raised when a particular transaction can not be found in the records."""
     def __init__(self, transaction_id: str):
         self.transaction_id=transaction_id
         super().__init__(f"Could not find the transaction-{transaction_id}, Enter a valid transaction_id")
+
 class InsufficientFundsError(FinanceError):
     """Raised when the amount entered is greater than the balance present in the account."""
     def __init__(self, amount:float, balance: float):
